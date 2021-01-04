f1ded4897c5e5997dceec1f8bc9d3bf3d5689fd0
Gsteiger, Sandro {MGPM~Basel} <sandro.gsteiger@roche.com>
2021-01-04 16:56:22 +0100

    Corrected bug in binary data prep.

--- Rpackages/gemtcPlus/R/process_binary.R
+++ Rpackages/gemtcPlus/R/process_binary.R
@@ -7,7 +7,12 @@
 #' @export
 process_binary <- function(data){
   
-  dmtc <- binary_data %>%
+  if(!("x" %in% names(data))) stop("Binary arm-level data must contain named column 'x'.")
+  if(!("n" %in% names(data))) stop("Binary arm-level data must contain named column 'n'.")
+  if(!("study" %in% names(data))) stop("Binary arm-level data must contain named column 'study'.")
+  if(!("treatment" %in% names(data))) stop("Binary arm-level data must contain named column 'treatment'.")
+  
+  dmtc <- data %>%
     dplyr::transmute(study, treatment, responders = x, sampleSize = n) %>%
     as.data.frame() # needed for nodesplit(.) function (fails for `tibble' input)
   
